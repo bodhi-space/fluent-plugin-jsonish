2afa5a7479685b26f26c696ba1d3e55c98fe5687
Alex Yamauchi <oss@hotschedules.com>
2017-08-15 20:49:24 +0000

    INFRA-5418: fixes to the logstash parser plugin.

--- lib/fluent/plugin/parser_logstash.rb
+++ lib/fluent/plugin/parser_logstash.rb
@@ -2,7 +2,7 @@ require_relative 'parser_jsonish'
 
 module Fluent
   class TextParser
-    class NodejsBunyanParser < JSONishParser
+    class LogstashParser < JSONishParser
       Plugin.register_parser('logstash', self)
 
       def configure(conf)
@@ -11,13 +11,11 @@ module Fluent
 
       def parse(text)
         super(text) do |time, record|
-          # Cannot be disabled in the library and serves no purpose,
-          # since it never varies.
           record.delete('@version')
           unless @message_key.nil? or @message_key == 'message'
             # Passing 'message_key' only duplicates the key to 'message' --
             # it does not delete the original key (which is unneeded).
-            record.delete(@message_key)
+            record.delete(@message_key) if record.key?(@message_key)
           end
           yield time, record
         end
