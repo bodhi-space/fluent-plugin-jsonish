7c81543cb42a0e2019b84ae9f41cb4522b023bcd
Alex Yamauchi <oss@hotschedules.com>
2017-08-16 23:59:40 +0000

    INFRA-5418: should be a working logstash parser

--- lib/fluent/plugin/parser_logstash.rb
+++ lib/fluent/plugin/parser_logstash.rb
@@ -2,22 +2,13 @@ require_relative 'parser_jsonish'
 
 module Fluent
   class TextParser
-    class NodejsBunyanParser < JSONishParser
-      Plugin.register_parser('nodejs_bunyan', self)
+    class NodejsLogstash < JSONishParser
+      Plugin.register_parser('logstash', self)
 
       def configure(conf)
         super(conf)
-        @time_key = 'time'
-        @message_key = 'msg'
-        @move_keys.update({ 'v' => nil, 'msg' => nil, 'hostname' => 'host' })
-      end
-
-      def parse(text)
-        super(text) do |time, record|
-          # Map the developer-defined levels to  syslog levels.
-          record['level'] = 8+(record['level']<=30?1:0)-(record['level']/10)
-          yield time, record
-        end
+        @time_key = '@timestamp'
+        @move_keys.update({ '@version' => nil, '@timestamp' => nil })
       end
 
     end
