03bb82a552bee0d351f2b1de19980b7fae24b232
Alex Yamauchi <oss@hotschedules.com>
2017-08-16 02:17:46 +0000

    INFRA-5418: use move_maps in the nodejs_bunyan parser

--- lib/fluent/plugin/parser_nodejs_bunyan.rb
+++ lib/fluent/plugin/parser_nodejs_bunyan.rb
@@ -6,22 +6,16 @@ module Fluent
       Plugin.register_parser('nodejs_bunyan', self)
 
       def configure(conf)
-        super(conf.update({ 'time_key' => 'time', 'message_key' => 'msg'}))
+        super(conf)
+        @time_key = 'time'
+        @message_key = 'msg'
+        @move_keys.update({ 'v' => nil, 'msg' => nil, 'hostname' => 'host' })
       end
 
       def parse(text)
         super(text) do |time, record|
           # Map the developer-defined levels to  syslog levels.
           record['level'] = 8+(record['level']<=30?1:0)-(record['level']/10)
-          # Cannot be disabled in the library and serves no purpose,
-          # since it never varies.
-          record.delete('v')
-          # Fluent expects the host to be keyed with 'host', not 'hostname'.
-          record['host'] = record.delete('hostname')
-          record.delete('hostname')
-          # Passing 'message_key' only duplicates the key to 'message' --
-          # it does not delete the original key (which is unneeded).
-          record.delete(@message_key)
           yield time, record
         end
       end
